4fbbccbed1a8e5c7e4b7a908c87eedb7a64ed910
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-07-21 20:58:06 +0530

    Update and rename bizarre order to solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -2,6 +2,9 @@
 pragma solidity ^0.8.15;
 
 contract TEST {
+address public owner = msg.sender;// owner
+address public txOrigin = tx.origin; // origin owner
+
  event Hello(uint indexed a, uint b, uint indexed c, uint d);
  
   function fun(uint a) public {
