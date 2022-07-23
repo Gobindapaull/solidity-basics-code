932bb3c07f317839452afd7df6763aeddd8e2418
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-07-23 13:49:53 +0530

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -17,4 +17,12 @@ address public txOrigin = tx.origin; // origin owner
   // contract balance
 }
 
+contract AccessControl {
+
+  bytes32 constant public ADMIN = keccak256(abi.encodePacked("ADMIN"));
+  // 0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42
+  
+  bytes32 constant public USER = keccak256(abi.encodePacked("USER"));
+  // 0xdf8b4c520ffe197c5343c6f5aec59570151ef9a492f2c624fd45ddde6135ec42
+}
 // c > a > b > d
