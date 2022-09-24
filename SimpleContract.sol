e587e83b541f0678ea27016d7badda6723912e3b
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-09-24 23:53:49 +0530

    Update SimpleContract.sol

--- SimpleContract.sol
+++ SimpleContract.sol
@@ -3,10 +3,14 @@
 pragma solidity ^0.8.4;
 
 contract SimpleContract {
-    
-    uint public favouriteNumber;
+
+    uint private favouriteNumber;
 
     function store(uint _favouriteNumber) external {
         favouriteNumber = _favouriteNumber;
+    }
+
+    function retrieve() external view returns(uint) {
+        return favouriteNumber;
     }
 }
