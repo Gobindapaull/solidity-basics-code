5d79fdf122d5b9f14d9b4ae1c62c483656982b70
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-10-30 15:22:25 +0530

    Update withdraw.sol

--- withdraw.sol
+++ withdraw.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.17;
 contract Test {
 
     address public owner = payable(msg.sender);
+    error UnAuthorized(address caller);
 
     function withdraw() public {
         if(msg.sender != owner) {
-            revert("not owner");
+            revert UnAuthorized(msg.sender);
         }
         payable(owner).transfer(address(this).balance);
     }
 
     receive() external payable {}
-}
+} 
