d51095285bd72a29647eb7c40530d0faa5878647
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-12-08 00:12:32 +0530

    Update modulo.sol

--- modulo.sol
+++ modulo.sol
@@ -6,4 +6,6 @@ contract Docs {
     int public y = int256(5) % int256(-2); // 1
     int public z = int256(-5) % int256(2); // -1
     int public q = int256(-5) % int256(-2); // -1
+    
+    uint public exp = 0 ** 0;
 }
