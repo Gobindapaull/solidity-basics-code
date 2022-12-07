ba6d942b176fdc29f80ad945d330908d578a83e4
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-12-07 23:52:34 +0530

    Create modulo.sol

---
+++ modulo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Docs {
+    int public x = int256(5) % int256(2); // 1
+    int public y = int256(5) % int256(-2); // 1
+    int public z = int256(-5) % int256(2); // -1
+    int public q = int256(-5) % int256(-2); // -1
+}
