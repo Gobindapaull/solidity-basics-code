25df225bc46d0f66a0de0757b9fa4e2ec6725eaf
Gobinda <80039050+Gobindapaull@users.noreply.github.com>
2022-12-14 16:07:10 +0530

    Create blockcoinbase.sol

---
+++ blockcoinbase.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Docs {
+    
+    address public coinbase = block.coinbase;
+    // 0x8945A1288dc78A6D8952a92C77aEe6730B414778
+    // 0x94D76E24F818426ae84aa404140E8D5F60E10E7e
+    // 0x0e9281e9C6a0808672EAba6bd1220E144C9bb07a
+
+}
