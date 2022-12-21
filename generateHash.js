dc2a08241214fcee41875f669719982cf53d60c1
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2022-12-22 03:34:13 +0530

    Update generateHash.js

--- generateHash.js
+++ generateHash.js
@@ -1,7 +1,7 @@
 const sha256 = require('crypto-js/sha256');
 
 class Block {
-    constructor(timestamp, data, previousHash) {
+    constructor(timestamp, data, previousHash = '') {
         this.timestamp = timestamp;
         this.data = data;
         this.previousHash = previousHash;
@@ -11,5 +11,44 @@ class Block {
         return sha256(this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
     }
 }
-const block = new Block('2022-12-22', { amount: 1500 }, 'abcd');
+class Blockchain {
+    constructor() {
+        this.chain = [this.generateGenesisBlock()];
+    }
+    generateGenesisBlock() {
+        return new Block('2022-12-22', 'GENESIS', '0000');
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+    isBlockchainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousHash = this.chain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+            if (currentBlock.previousHash !== previousHash.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+const coin = new Blockchain();
+const block = new Block('2022-12-22', { price: 1500 }, 'abcd');
+coin.addBlock(block);
+
 console.log(block);
+console.log(coin);
+
+console.log(coin.isBlockchainValid());
+coin.chain[1].data = "HACKED";
+console.log(coin);
+console.log('hacked blockchain' + coin.isBlockchainValid());
