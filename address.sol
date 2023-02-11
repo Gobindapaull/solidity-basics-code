2fd1e64b2a967e815d171fed98b54c3793256a49
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-02-12 02:14:06 +0530

    Create address.sol

---
+++ address.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18; 
+
+contract Address {
+    address public minusAddress = address(0); // null address
+    address public oneAddress = address(1); // null address
+    address public twoAddress = address(2); // null address
+    address public threeAddress = address(3); // null address
+    address public fourAddress = address(4); // null address
+    address public fiveAddress = address(5); // null address
+    address public randomAddress = address(4789374873973749374);
+}
