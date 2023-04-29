80626bfe59f32fe749b8ef84d0c2c467619a3fb4
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-04-30 01:47:07 +0530

    Update basic.js

--- web3/basic.js
+++ web3/basic.js
@@ -9,6 +9,26 @@ web3.eth.getBlock('latest').then(console.log)
 
 // latest block hash
 web3.eth.getBlock('latest').then((block) => {
-    console.log(block.hash)
+    console.log({
+        blockHash: block.hash,
+        blockNumber: block.number
+    })
 })
+// latest 10 block hash and number
+web3.eth.getBlockNumber().then((latest) => {
+    for (i = 0; i < 10; i++) {
+        web3.eth.getBlock(latest - i).then((block) => {
+            console.log({
+                blockHash: block.hash,
+                blockNumber: block.number
+            })
+        })
+    }
+})
+
+// total transactions in a block
+web3.eth.getBlockTransactionCount('latest').then(console.log)
+
+hash = '0x4038f53f6b47e929c36599cfc952cdd95dca3db358f6ba2eac6c13966e3bdf00'
+web3.eth.getTransactionFromBlock(hash, 3).then(console.log)
 
