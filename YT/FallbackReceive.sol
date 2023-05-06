cef2e50e11542cc99a8c553abecc0489a5059778
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-06 11:08:26 +0530

    Update FallbackReceive.sol

--- YT/FallbackReceive.sol
+++ YT/FallbackReceive.sol
@@ -3,22 +3,14 @@ pragma solidity ^0.8.18;
 
 contract Fallback {
 
-    event Log(address sender, uint value, bytes data);
+    event Log(string name, address sender, uint value, bytes data);
 
     fallback() external payable {
-        emit Log(msg.sender, msg.value, msg.data);
+        emit Log("Fallback function", msg.sender, msg.value, msg.data);
     }
-    // "event": "Log",
-	// 	"args": {
-	// 		"0": "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", 
-	// 		"1": "2000000000000000000",
-	// 		"2": "0x12",
-	// 		"sender": "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4",
-	// 		"value": "2000000000000000000",
-	// 		"data": "0x12"
-	// 	}
+
     receive() external payable {
-        emit Log(msg.sender, msg.value, "");
+        emit Log("Receive function", msg.sender, msg.value, "");
         // "msg.data" cannot be used inside of "receive" function
     }
 
