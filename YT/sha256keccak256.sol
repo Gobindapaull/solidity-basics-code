aaecc1689deaa1429a745b1ba107e163bb9bfe70
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-07 09:10:23 +0530

    Update sha256keccak256.sol

--- YT/sha256keccak256.sol
+++ YT/sha256keccak256.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.8.18;
 
 contract Keccak256 {
 
-    function Keccak256Fun() public pure returns (bytes32 result) {
+    function Keccak256Fun() public pure returns (bytes32) {
         return keccak256("SOLIDITY"); // 0x6de1c330c658139e756b237a5741f422c7e56f5f05f25fc83e2d3289e1356537
     }
 
-    function sha256Fun() public pure returns (bytes32 result) {
+    function sha256Fun() public pure returns (bytes32) {
         return sha256("SOLIDITY"); // 0xe02e2fed1ccfb1fb088ce50d808a160eb355bdff5e65e266a065b332f4eb2f97
     }
     
