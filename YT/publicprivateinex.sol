4bc0476bdaebf2043d66529ad1c3974a8517eb2d
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-07 10:31:34 +0530

    Create publicprivateinex.sol

---
+++ YT/publicprivateinex.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+contract Advance {
+    uint private x = 1;
+    uint public y = 2; // y visible on etherscan/bscscan
+    uint internal z = 3;
+
+    function privateFun() private pure returns (string memory) {
+        return "private function";
+    }
+
+    function publicFun() public pure returns (string memory) {
+        return "public function"; // visible on etherscan/bscscan
+    }
+
+    function internalFun() internal pure returns (string memory) {
+        return "Internal function";
+    }
+
+    function externalFun() external pure returns (string memory) {
+        return "External function"; // visible on etherscan/bscscan
+    }
+}
