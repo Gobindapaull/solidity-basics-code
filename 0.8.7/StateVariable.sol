9a3a8d0f507f556ab7b6c5ff26aebf234589043a
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-05-19 13:53:11 +0530

    Create StateVariable.sol

---
+++ 0.8.7/StateVariable.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+contract StateVariable {
+    uint public stateVariable = 123;
+
+    function LocalVar() public {
+        uint localVariable = 456;
+    }
+}
