4f9cd330095c07c4c189d5fc1300ff3461fee3a9
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-21 13:55:34 +0530

    Create mask.sol

---
+++ src/mask.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+contract Mask {
+    uint public constant oneZero = 1 << 255; // 57896044618658097711785492504343953926634992332820282019728792003956564819968
+    uint public constant zeroOne = 255 << 1; // 510
+}
+    
