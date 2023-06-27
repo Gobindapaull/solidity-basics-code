c1cbd66b0c39649033b21c5c2bcf8e6e0d58b4ed
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-27 23:52:20 +0530

    Update wallet.js

--- ethers/wallet.js
+++ ethers/wallet.js
@@ -6,3 +6,7 @@ console.log('signer: ', signer.address)
 // or wallet
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
 console.log('wallet address: ', wallet.address)
+
+// create random wallet
+const randomWallet = ethers.Wallet.createRandom(provider)
+console.log('random wallet address: ',randomWallet.address)
