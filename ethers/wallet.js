0f9b9fe19b5722113d5949f44103adcc7a68e24c
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-27 23:48:53 +0530

    Update wallet.js

--- ethers/wallet.js
+++ ethers/wallet.js
@@ -2,3 +2,7 @@
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
 const signer = wallet.connect(provider)
 console.log('signer: ', signer.address)
+
+// or wallet
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+console.log('wallet address: ', wallet.address)
