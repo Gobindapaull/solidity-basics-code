d0ebde3c6fd9193a1105e23da7a4a549498d51ec
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-06-27 22:36:23 +0530

    Update chaindId&balance.js

--- ethers/chaindId&balance.js
+++ ethers/chaindId&balance.js
@@ -5,8 +5,20 @@ const provider = new ethers.JsonRpcProvider(process.env.INFURA_URL)
 
 // Chain Id
 const network = provider.getNetwork()
-network.then((network) => console.log('chainId: ',network.chainId.toString()))
+network.then((network) => console.log('chainId: ', network.chainId.toString()))
+
+// block number
+const blockNumber = provider.getBlockNumber()
+blockNumber.then((blockNo) => console.log(blockNo))
 
 // Vitalik eth balance
 const bal = provider.getBalance("vitalik.eth")
 bal.then((balance) => console.log('balance: ', ethers.formatEther(balance)))
+
+// nonce
+const nonce = provider.getTransactionCount("vitalik.eth")
+nonce.then((nonce) => console.log(nonce))
+
+// getTransactionReceipt
+const getTransactionReceipt = provider.getTransactionReceipt("0xa9c9f00a7e9fac43a22d1f402dad47a39e6d366adfb9a820addad5aaa25cc060")
+getTransactionReceipt.then((gtr) => console.log(gtr))
