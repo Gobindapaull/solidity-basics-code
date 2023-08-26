f6baea750cb9f2c80a916238e158e867d531b2e8
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-08-27 02:02:23 +0530

    Create transfer.js

---
+++ ethers/transfer/transfer.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const ethers = require('ethers')
+
+const ABI = require('./abi.json')
+
+const privateKey = process.env.PRIVATE_KEY
+const url = process.env.URL
+const tokenAddress = process.env.tokenAddress
+
+const provider = new ethers.JsonRpcProvider(url)
+const wallet = new ethers.Wallet(privateKey)
+const signer = wallet.connect(provider)
+const tokenContract = new ethers.Contract(tokenAddress, ABI, signer)
+
+const receiver = ethers.Wallet.createRandom().address
+const amount = 1000;
+
+const start = async () => {
+    const name = await tokenContract.name()
+    console.log('token name: ', name)
+
+    for ( let i = 0; i < 20; i++) {
+        const transfe = await tokenContract.transfer(receiver, amount)
+        const transfer = await transfe.wait()
+        console.log('Tx hash: ', transfer.hash)
+        console.log(`approved ${amount} tokens`)
+    }
+}
+start()
+
