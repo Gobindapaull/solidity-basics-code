ca47e880251ec13602be36f9ad6e2ef55de35dd7
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-09-25 03:35:09 +0530

    Create ASCII.sol

---
+++ src/ASCII.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+//   ____   ___  _     ___ ____ ___ _______   __
+//  / ___| / _ \| |   |_ _|  _ \_ _|_   _\ \ / /
+//  \___ \| | | | |    | || | | | |  | |  \ V / 
+//   ___) | |_| | |___ | || |_| | |  | |   | |  
+//  |____/ \___/|_____|___|____/___| |_|   |_|  
+
+// Telegram: @autoboyt
+
+
+contract Text {
+    string public telegram = "autoboyt";
+}
