8ce912b02aa8b7acdac85015abc2306a6b141f27
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-10-28 02:18:23 +0530

    Update Lottery.sol

--- smartContracts/Lottery.sol
+++ smartContracts/Lottery.sol
@@ -1,1 +1,28 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+contract ERC20 {
+    address public manager;
+    address payable[] public players;
+    address payable winner;
+    uint public num;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    function participate() public payable {
+        require(msg.value == 1 ether, "Please pay 1 ether to participate");
+        players.push(payable(msg.sender));
+    }
+
+    function getContractBalance() public view returns (uint256) {
+        require(msg.sender == manager, "only manager can view it");
+        return address(this).balance;
+    }
+
+    function random() public view returns (uint) {
+       return uint(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, players.length)));
+    }
+}
