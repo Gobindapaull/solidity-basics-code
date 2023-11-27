a5ed5d000d8f0ecc85e470d279ee14bfd77311ab
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-11-28 03:52:15 +0530

    Update router.js

--- pancakeswap/router.js
+++ pancakeswap/router.js
@@ -1,13 +1,15 @@
 const ethers = require('ethers')
 require('dotenv').config()
+
 const pancakeswapABI = require('./pancakeswapABI.json')
+const pancakeswapRouterAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
 
 const bsc = process.env.BSC_NODE;
-const mnemonic = process.env.YOUR_MNEMONIC;
 const provider = new ethers.JsonRpcProvider(bsc);
+
+const mnemonic = process.env.YOUR_MNEMONIC;
 const wallet = ethers.Wallet.fromPhrase(mnemonic);
 const account = wallet.connect(provider);
-const pancakeswapRouterAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E";
 
 const router = new ethers.Contract(
     pancakeswapRouterAddress,
