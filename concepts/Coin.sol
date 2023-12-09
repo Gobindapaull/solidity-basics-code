cff6e3b32468c28c4ad90cf8b42663c4378abc94
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2023-12-09 09:00:05 +0530

    Update Coin.sol

--- concepts/Coin.sol
+++ concepts/Coin.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
 
+contract Coin {
+    address public minter;
+    mapping(address => uint256) public balances;
+    event Sent(address from, address to, uint256 amount);
+    error InsufficientBalance(uint256 requested, uint256 available);
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+    function mint(address receiver, uint256 amount) public {
+        require(msg.sender == minter, "not owner");
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint256 amount) public {
+        if (amount > balances[msg.sender]) {
+            revert InsufficientBalance({
+                requested: amount,
+                available: balances[msg.sender]
+            });
+        }
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
