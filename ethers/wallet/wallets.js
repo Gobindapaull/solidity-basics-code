ca6545e0461228b0bd7fdd8f5a638f32a68a7e30
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-01-27 17:43:14 +0530

    Update wallets.js

--- ethers/wallet/wallets.js
+++ ethers/wallet/wallets.js
@@ -11,6 +11,11 @@ console.log(walletRandom.privateKey)
 const walletFromPhrase = ethers.Wallet.fromPhrase("half occur build misery shrug believe spoon owner plug diary pitch civil")
 console.log(walletFromPhrase.address)
 console.log(walletFromPhrase.privateKey)
+console.log(walletFromPhrase.provider)
 
 const walletFromPrivateKey = new ethers.Wallet("0x6c2202b77b6b6c2cdd6546e3ece91fb719518167e5d69c8075fde96dea080e77")
 console.log(walletFromPrivateKey.address)
+
+
+
+
