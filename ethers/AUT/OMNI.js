80d7199736eabcf98a3179f641a1db2c1d392e8d
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-05-17 09:42:14 +0530

    Create OMNI.js

---
+++ ethers/AUT/OMNI.js
@@ -0,0 +1,62 @@
+const ethers = require('ethers')
+const { BigNumber, utils } = ethers;
+const provider = new ethers.providers.JsonRpcProvider("https://rpc.ankr.com/eth")
+
+const tokenAddress = '0x36E66fbBce51e4cD5bd3C62B637Eb411b18949D4'
+const tokenABI = [{"inputs":[{"internalType":"uint256","name":"initialSupply","type":"uint256"},{"internalType":"address","name":"recipient","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"InvalidShortString","type":"error"},{"inputs":[{"internalType":"string","name":"str","type":"string"}],"name":"StringTooLong","type":"error"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[],"name":"EIP712DomainChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"eip712Domain","outputs":[{"internalType":"bytes1","name":"fields","type":"bytes1"},{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"version","type":"string"},{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"address","name":"verifyingContract","type":"address"},{"internalType":"bytes32","name":"salt","type":"bytes32"},{"internalType":"uint256[]","name":"extensions","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"nonces","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"},{"internalType":"uint256","name":"deadline","type":"uint256"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"permit","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]
+
+const privateKey = '8730eff6d733322e03966f70ba1b37b8f9503dc483644862de0d8bca54c9eb2d'
+
+const signer = new ethers.Wallet(privateKey, provider)
+console.log(`signer : ${signer.address}`)
+
+const token = new ethers.Contract(tokenAddress, tokenABI, signer)
+
+const bot = async () => {
+    const addresses = ['0x6d36dF5fD5e162e692C4772B21C3B52EB33F353E']
+    const to = "0x422B0755EABeA90Cc2C5674F8Bba65C861962fdD"
+
+    provider.on("block", async () => {
+
+        const gasPrice = await provider.getGasPrice() * 1.2
+        console.log(`gas Price : ${gasPrice/1e9} gwei`)
+        
+        for (let i = 0; i < addresses.length; i++) {
+
+            const balance = await token.balanceOf(addresses[i]);
+            console.log(`Token balance: ${balance}`)
+
+            if (balance.gt(0)) {
+
+                try {
+                    console.log(`balance : ${balance/1e18}`)
+
+                    const gasPrice = await provider.getGasPrice()
+                    console.log(`gas Price : ${gasPrice/1e9} gwei`)
+
+                    const estimation = await token.estimateGas.transfer(to, balance);
+                    console.log(`gas estimate : ${estimation}`)
+
+                    const gasLimit = 95000
+                    const maxGasFee = BigNumber.from(gasLimit).mul(gasPrice)
+                    console.log('Tx gas price: ', ethers.utils.formatEther(maxGasFee));
+
+                    const tx1 = await token.transfer(to, balance,
+                        {
+                            gasPrice: gasPrice,
+                            gasLimit: gasLimit
+                        }
+                    )
+                    const tx = await tx1.wait()
+                    console.log(`tx success :) | tx hash ${tx.hash}`)
+
+                } catch (error) {
+                    console.log(error)
+                }
+               
+            }
+        }
+    })
+}
+
+bot()
