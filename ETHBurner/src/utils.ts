5bc9b57e48f8bc4bbeff7baf1e3bc2e2bbc2e8b8
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-06-08 20:07:59 +0530

    Delete ETHBurner/src directory

--- ETHBurner/src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { BigNumber } from "@ethersproject/bignumber";
-
-const GWEI = 1e9;
-
-/** Returns human-readable gas price in gwei. */
-export const gasPriceToGwei = (gasPrice: BigNumber) => (
-    gasPrice.mul(100).div(GWEI).toNumber() / 100
-);
