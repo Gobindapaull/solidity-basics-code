ea0758aff450ff09b1666d4d03a7b33939570e34
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-06-17 03:37:14 +0530

    Create GasCost.sol

---
+++ 0.8.25/assembly/GasCost.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+contract GasCost {
+    function alloc_mem(uint256 n) external view returns (uint256) {
+        uint256 gas_start = gasleft();
+        uint256[] memory arr = new uint256[](n);
+        uint256 gas_end = gasleft();
+        return gas_start - gas_end;
+    }
+}
