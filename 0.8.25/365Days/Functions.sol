5a07cecba3f1e8387036b645899f5a3598e8b0c8
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-06-18 04:32:46 +0530

    Create Functions.sol

---
+++ 0.8.25/365Days/Functions.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract Functions {
+    function MultiDimDynamicSizeArray(uint256[][] memory arr) public {
+
+    }
+
+    function arrayInput(uint256[] memory arr) public {
+
+    }
+
+    function returnMultipleValues() public pure returns (uint256, bool, uint256) {
+        return (1, true, 379);
+    }
+}
