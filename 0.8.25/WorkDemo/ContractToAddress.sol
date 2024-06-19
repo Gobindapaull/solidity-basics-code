ebdb123797b610a1b6fff2559f1d02ccc629cb19
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-06-19 19:24:39 +0530

    Update ContractToAddress.sol

--- 0.8.25/WorkDemo/ContractToAddress.sol
+++ 0.8.25/WorkDemo/ContractToAddress.sol
@@ -2,9 +2,14 @@
 pragma solidity ^0.8.25;
 
 contract Sent {
+
     address  receiver = payable(0x422B0755EABeA90Cc2C5674F8Bba65C861962fdD);
     
     receive() external payable {
         payable(receiver).transfer(msg.value);
     }
+
+    function balance(address _owner) public view returns (uint256) {
+        return _owner.balance;
+    } 
 }
