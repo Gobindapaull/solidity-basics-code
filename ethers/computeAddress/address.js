5c8c44028901f50a57bd5e81e78cdd4e8c5eafcd
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-06-26 06:39:53 +0530

    Create address.js

---
+++ ethers/computeAddress/address.js
@@ -0,0 +1,7 @@
+const ethers = require("ethers")
+const computeAddress = ethers.computeAddress("0xa8185ef5a8610adfc9f32930ac9c17ec20799ef80baa03ab209cf7a6df1f5b2c")
+console.log(computeAddress) // 0xf0CD809234B2B5B42230F43533A80b04F23083eE
+
+// "dependencies": {
+//     "ethers": "^6.13.1"
+//   }
