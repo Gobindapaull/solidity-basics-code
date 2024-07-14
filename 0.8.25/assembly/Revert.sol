ce449cff44562e515179f7982d2bd07a7bd5d7dd
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-07-14 12:20:31 +0530

    Update Revert.sol

--- 0.8.25/assembly/Revert.sol
+++ 0.8.25/assembly/Revert.sol
@@ -1,16 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-
-contract MemRevert {
-
-    function test_revert_with_error_message() public pure{
+contract Revert {
+    function revert_test() public pure {
         assembly {
             let p := mload(0x40)
             mstore(p, shl(224, 0x08c379a0))
             mstore(add(p, 0x04), 0x20)
-            mstore(add(p, 0x24), 5)
-            mstore(add(p, 0x44), "ERROR")
+            mstore(add(p, 0x24), 13)
+            mstore(add(p, 0x44), "ERROR MESSAGE")
             revert(p, 0x64)
         }
     }
