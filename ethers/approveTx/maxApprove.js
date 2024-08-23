c1dc1c60eaef988e3e41473921060ffa7b4eeed3
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-08-23 23:31:20 +0530

    Create maxApprove.js

---
+++ ethers/approveTx/maxApprove.js
@@ -0,0 +1,5 @@
+import { ethers } from "ethers" //  "ethers": "^6.13.2"
+
+// "type": "module"
+
+console.log(ethers.MaxUint256) // 115792089237316195423570985008687907853269984665640564039457584007913129639935n
