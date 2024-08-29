4f2421cfd78de7bc6e3efc4666bf7983c36cf1cb
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-08-30 01:24:34 +0530

    Update crypto.js

--- NPM Package/crypto/crypto.js
+++ NPM Package/crypto/crypto.js
@@ -11,10 +11,11 @@ const main = async () => {
     const id = crypto.randomBytes(32)
     const key = id.toString("hex")
     const idLen = id.length
-    const wallet = new ethers.Wallet(key)
-    
+    const wallet = new ethers.Wallet(key, provider)
+
     console.log(key) // 944d304f4924a77339326d15b72087605d43f878ab1361251d527732e8b2f867
     console.log(wallet.address)
+    console.log(await provider.getBalance(wallet.address))
     console.log(idLen)
     // TronLink = THdoRo7VNcJFdDtbkcLjHPSKTMwYbrqQ8y
 
