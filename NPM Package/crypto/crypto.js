31c2d4cb0ad03278d2712fad626b25aaa310d7d7
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-08-30 00:19:41 +0530

    Create crypto.js

---
+++ NPM Package/crypto/crypto.js
@@ -0,0 +1,23 @@
+const ethers = require("ethers")
+const crypto = require("crypto")
+
+
+const main = async () => {
+    const provider = new ethers.JsonRpcProvider("https://rpc.ankr.com/eth")
+
+    const getBlockNumber = await provider.getBlockNumber()
+    console.log(getBlockNumber)
+
+    const id = crypto.randomBytes(32)
+    const key = id.toString("hex")
+    const idLen = id.length
+    const wallet = new ethers.Wallet(key)
+    
+    console.log(key) // 944d304f4924a77339326d15b72087605d43f878ab1361251d527732e8b2f867
+    console.log(wallet.address)
+    console.log(idLen)
+    // TronLink = THdoRo7VNcJFdDtbkcLjHPSKTMwYbrqQ8y
+
+}
+
+main()
