e1bcd8dd4946ef3a82ec3295ce6e12d99a311e74
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-09-10 00:08:53 +0530

    Update uniswap-sdk.js

--- NPM Package/uniswap/uniswap-sdk.js
+++ NPM Package/uniswap/uniswap-sdk.js
@@ -1,7 +1,7 @@
-import { Percent } from "@uniswap/sdk"
+import { Percent } from "@uniswap/sdk";
 
-const SLIPPAGE = new Percent("50", "10000")
-const deadline = Math.floor(Date.now() / 1000)
+const SLIPPAGE = new Percent("50", "10000");
+const deadline = Math.floor(Date.now() / 1000);
 
-console.log(`The current Unix epoch time is: ${deadline}`)
-console.log(`SLIPPAGE: ${SLIPPAGE.toSignificant()}`)
+console.log(`The current Unix epoch time is: ${deadline}`);
+console.log(`SLIPPAGE: ${SLIPPAGE.toSignificant()}`);
