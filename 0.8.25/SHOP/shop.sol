3d9940f2368891d574f355ab03964a7984dabb98
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-10-04 01:34:08 +0530

    Update shop.sol

--- 0.8.25/SHOP/shop.sol
+++ 0.8.25/SHOP/shop.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
+contract Shop {
+    uint256 public price = 2 ether;
+    address public owner;
+    address public shopAddress;
+    bool fullyPaid;
+
+    mapping(address => bool) public buyers;
+
+    constructor () {
+        owner = msg.sender;
+        shopAddress = address(this);
+    }
+
+    function addBuyer(address _addr) public {
+        require(owner == msg.sender, "Not owner");
+        buyers[_addr] = true;
+    }
+
+    function getBalance() public view returns(uint256) {
+        return shopAddress.balance;
+    }
+
+    receive() external payable {
+        require(buyers[msg.sender] && msg.value >= price, "you can't buy this item");
+    }
+
+    function withdrawAll() public {
+        require(owner == msg.sender && shopAddress.balance > 0, "Rejected");
+        address payable receiver = payable(msg.sender);
+        receiver.transfer(shopAddress.balance);
+    }
+}
