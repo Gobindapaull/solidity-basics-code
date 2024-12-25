98ca5125f8573c5f566d0a3f7085eb34a6edb184
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2024-12-25 17:37:27 +0530

    Create index.ts

---
+++ Hardhat/getSigner/index.ts
@@ -0,0 +1,16 @@
+import { ethers } from "ethers";
+
+async function main() {
+    try {
+        const url = "http://127.0.0.1:8545/";
+        const provider = new ethers.JsonRpcProvider(url);
+        const signer = await provider.getSigner();
+        const balance = await provider.getBalance(signer.address);
+        const formattedBalance = ethers.formatEther(balance);
+        console.log(`Wallet balance : ${formattedBalance} ETH`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+main();
