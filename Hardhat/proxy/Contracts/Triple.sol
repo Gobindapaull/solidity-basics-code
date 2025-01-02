6c458deaac01933c83993d8e90f3a83f5ac30ae3
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-01-03 02:26:02 +0530

    Update Triple.sol

--- Hardhat/proxy/Contracts/Triple.sol
+++ Hardhat/proxy/Contracts/Triple.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
 
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Triple is Initializable {
+    function initialize() public initializer{
+
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+    
+    function triple(uint256 _number) public pure returns (uint256) {
+        // it's a mistake
+        return _number * 2;
+    }
+}
