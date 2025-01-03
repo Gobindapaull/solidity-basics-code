d72ff17929b0190d8c0c8b283d0c5770e15fc8e3
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-01-04 01:06:11 +0530

    Update AccessControlContract.sol

--- Hardhat/AccessControl/contracts/AccessControlContract.sol
+++ Hardhat/AccessControl/contracts/AccessControlContract.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AccessControlContract is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
+    constructor() ERC20("MyToken", "MT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+}
 
