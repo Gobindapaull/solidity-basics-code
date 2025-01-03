43f8fb9ed0978464c27f6527c9e18bef4c544d38
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-01-03 21:32:28 +0530

    Update MyContract.sol

--- Hardhat/Ownable/MyContract.sol
+++ Hardhat/Ownable/MyContract.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
 
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyContract is Ownable {
+    uint256 public number;
+
+    constructor() Ownable(_msgSender()) {
+        number = 123;
+    }
+
+    function setNumber(uint256 _number) public onlyOwner {
+        number = _number;
+    }
+
+}
