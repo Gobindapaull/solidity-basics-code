63c05040f18cfe6f9b8695b2d36d35c096ae3482
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-01-08 17:29:35 +0530

    Create Token.sol

---
+++ 0.8.25/unicode/Token.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Token {
+    string public constant _name = unicode"Gulf of America";
+    string public constant _symbol = unicode"GOA";
+}
