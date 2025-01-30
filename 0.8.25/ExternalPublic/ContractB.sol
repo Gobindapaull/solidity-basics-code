bab26aad9e8cf42fed9beb253895b175721be098
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-01-30 17:05:51 +0530

    Update ContractB.sol

--- 0.8.25/ExternalPublic/ContractB.sol
+++ 0.8.25/ExternalPublic/ContractB.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+import "./ContractA.sol";
+
+contract ContractB {
+    ContractA public contractA;
+
+    constructor(address _contractA) {
+        contractA = ContractA(_contractA);
+    }
+
+    function callingExternalFunction() public {
+        contractA.externalFunction();
+        contractA.publicFunction();
+    }
+}
