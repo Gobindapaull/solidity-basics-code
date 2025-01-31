f12472a9529a16c25b8188c578dc1d9a78db0033
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-01-31 17:52:56 +0530

    Create Class90.sol

---
+++ 0.8.25/365Days/Class90.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+contract Class90 {
+    // https://ethportal.net/
+    // https://sketchpad.app/en/
+    // State bloat, statelessness, verkle trees
+    // Hexary tree
+    // sha256 or blake
+    // 
+}
