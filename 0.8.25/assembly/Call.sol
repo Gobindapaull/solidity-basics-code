3b911a3cbc05909def62c180c4849115a5cc1cd0
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-03-02 15:29:12 +0530

    Update Call.sol

--- 0.8.25/assembly/Call.sol
+++ 0.8.25/assembly/Call.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
+pragma solidity ^0.8.23;
 
 contract Counter {
     uint256 public count;
@@ -11,26 +11,38 @@ contract Counter {
 }
 
 contract YulCall {
-    function yul_call(address addr, bytes calldata data) external returns (bytes memory) {
+    function yul_call(address addr, bytes calldata data) external 
+    returns (bytes memory) {
         assembly {
+            // Load free memory pointer
             let p := mload(0x40)
+            // Copy calldata to memory
             calldatacopy(p, data.offset, data.length)
+            // Call counter
             let ok := call(gas(), addr, 0, p, data.length, 0, 0)
+            // Revert if call is not successful
             if iszero(ok) {
-                revert(0, 0)
+                revert (0, 0)
             }
+            // Get return data size
             let return_data_size := returndatasize()
+            // Return data as ABI encoded bytes
+            // Store offset
             mstore(p, 0x20)
+            // Store length
             mstore(add(p, 0x20), return_data_size)
+            // Copy data from returndata
             returndatacopy(add(p, 0x40), 0, return_data_size)
-            return (p, add(0x40, return_data_size))
+            // Return data
+            return(p, add(0x40, return_data_size))
         }
     }
 }
 
 contract Test {
-    function test_inc(address yul, address counter) public returns (uint256 count) {
+    function test_inc(address yul, address counter) public 
+    returns (uint256 count) {
         bytes memory res = YulCall(yul).yul_call(counter, abi.encodeCall(Counter.inc, ()));
         count = abi.decode(res, (uint256));
-    } 
+    }
 }
