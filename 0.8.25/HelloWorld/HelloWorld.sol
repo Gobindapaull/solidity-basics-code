c54040b2ffa7231977fa79b755cb068f15ae6c51
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-03-04 16:23:32 +0530

    Update HelloWorld.sol

--- 0.8.25/HelloWorld/HelloWorld.sol
+++ 0.8.25/HelloWorld/HelloWorld.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+contract HelloWorld {
+    string public name = "Hello World";
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    function setName(string memory _newName) public {
+        require(msg.sender == owner, "Not owner");
+        name = _newName;
+    }
+}
