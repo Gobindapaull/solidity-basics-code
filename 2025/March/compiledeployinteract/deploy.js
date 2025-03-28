ec709dadf1318705d889e9552a52e3d167e8ac0b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-03-28 22:55:42 +0530

    Create deploy.js

---
+++ 2025/March/compiledeployinteract/deploy.js
@@ -0,0 +1,21 @@
+const ethers = require("ethers")
+const fs = require("fs")
+require("dotenv").config()
+
+const provider = new ethers.JsonRpcProvider("https://data-seed-prebsc-1-s1.binance.org:8545/")
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+console.log(`wallet address : ${wallet.address}`)
+
+
+const contractABI = JSON.parse(fs.readFileSync("./build/TimeLock_sol_TimeLock.abi", "utf8"))
+const contractBytecode = fs.readFileSync("./build/TimeLock_sol_TimeLock.bin", "utf8")
+
+const deploy = async () => {
+    const factory = new ethers.ContractFactory(contractABI, "0x" + contractBytecode, wallet)
+    const contract = await factory.deploy("")
+
+    await contract.waitForDeployment()
+    console.log(`Deployed contract address: ${contract.target}`)
+}
+
+deploy()
