ea0add0e327e7ab8b2dac7f69d356d548192fadb
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-03-28 22:57:02 +0530

    Update TimeLock.sol

--- 2025/March/compiledeployinteract/TimeLock.sol
+++ 2025/March/compiledeployinteract/TimeLock.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
 
+contract TimeLock {
+    address payable public beneficiary;
+    uint256 public releaseTime;
+
+    constructor(address payable _beneficiary) {
+        beneficiary = _beneficiary;
+        releaseTime = block.timestamp + 10 seconds;
+    }
+
+    function deposit() public payable {}
+
+    function withdraw() public {
+        require(block.timestamp >= releaseTime, "Funds are locked");
+        require(address(this).balance > 0, "No funds available");
+        beneficiary.transfer(address(this).balance);
+    }
+}
