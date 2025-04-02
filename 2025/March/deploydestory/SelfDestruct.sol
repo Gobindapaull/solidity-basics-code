2b3513731b7f3ca9c08c011e6b866c252aaf63f8
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-04-02 08:00:16 +0530

    Update SelfDestruct.sol

--- 2025/March/deploydestory/SelfDestruct.sol
+++ 2025/March/deploydestory/SelfDestruct.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SelfDestructContract {
+    address payable owner;
+    uint256 public selfDestructTime;
+
+    constructor(uint256 _time) {
+        owner = payable(msg.sender);
+        selfDestructTime = block.timestamp + _time;
+    }
+
+    function destroy() public {
+        require(block.timestamp >= selfDestructTime, "Not time yet");
+        selfdestruct(owner);
+    }
+
+    receive() external payable {}
+}
