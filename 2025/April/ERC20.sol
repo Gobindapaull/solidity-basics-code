6d040eac16c2d08ecca337c544e4cf96f2de5565
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-04-30 07:08:04 +0530

    Update ERC20.sol

--- 2025/April/ERC20.sol
+++ 2025/April/ERC20.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.29;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("Token Name", "SYM") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
