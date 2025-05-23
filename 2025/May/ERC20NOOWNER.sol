015a8f93c859285f699247b5fee5a6f8658d4588
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-05-23 12:45:56 +0530

    Update ERC20NOOWNER.sol

--- 2025/May/ERC20NOOWNER.sol
+++ 2025/May/ERC20NOOWNER.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+contract MyToken is ERC20 {
+    address devWallet = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
+    constructor() ERC20("Test Token", "TST") {
+        _mint(msg.sender, 10000000 * 10 ** decimals()); // 10 million tokens
+        _mint(devWallet, 1000000 * 10 ** decimals()); // 1 million tokens
+    }
+}
+
+// Contract have no owner and ownership tranfser function
