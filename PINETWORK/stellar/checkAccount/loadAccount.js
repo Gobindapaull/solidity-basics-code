022e737afb86a83bdf1f0fdeece3d99a6e6943a5
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-07-07 04:05:04 +0530

    Create loadAccount.js

---
+++ PINETWORK/stellar/checkAccount/loadAccount.js
@@ -0,0 +1,18 @@
+const stellar = require("@stellar/stellar-sdk");
+const server = new stellar.Horizon.Server("https://api.mainnet.minepi.com");
+const publicKey = "GD5HGPHVL73EBDUD2Z4K2VDRLUBC4FFN7GOBLKPK6OPPXH6TED4TRK73";
+
+async function checkAccount() {
+    try {
+        const account = await server.loadAccount(publicKey);
+        account.balances.forEach((balance) => {
+            console.log(`Asset : ${balance.asset_type}, Balance : ${balance.balance}`);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+checkAccount();
+
+
