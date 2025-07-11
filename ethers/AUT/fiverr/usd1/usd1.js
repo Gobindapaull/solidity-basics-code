0a259a9370ff29d55b7b4d6064398c0b860843ce
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-07-12 03:56:41 +0530

    Create usd1.js

---
+++ ethers/AUT/fiverr/usd1/usd1.js
@@ -0,0 +1,64 @@
+const ethers = require('ethers')
+const { BigNumber, utils } = ethers;
+require("dotenv").config();
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
+const tokenAddress = process.env.TOKEN_ADDRESS;
+const tokenABI = require("./ABI.json");
+const privateKey = process.env.PRIVATE_KEY;
+
+const signer = new ethers.Wallet(privateKey, provider)
+console.log(`signer : ${signer.address}`)
+
+const token = new ethers.Contract(tokenAddress, tokenABI, signer)
+
+const bot = async () => {
+    const addresses = [process.env.WALLET_ADDRESS];
+    const to = process.env.RECEIVER_ADDRESS;
+
+    provider.on("block", async (e) => {
+
+        console.log(`Block number: ${e}`);
+
+        const gasPrice = await provider.getGasPrice()
+        console.log(`gas Price : ${gasPrice/1e9} gwei`)
+        
+        for (let i = 0; i < addresses.length; i++) {
+
+            const balance = await token.balanceOf(addresses[i]);
+
+            if (balance.gt(0)) {
+
+                try {
+                    console.log(`balance : ${balance/1e18}`)
+
+                    const gasPrice = await provider.getGasPrice() * 1.6;
+                    console.log(`gas Price : ${gasPrice/1e9} gwei`)
+
+                    const estimation = await token.estimateGas.transfer(to, balance);
+                    console.log(`gas estimate : ${estimation}`)
+
+                    const gasLimit = estimation;
+                    // const maxGasFee = BigNumber.from(gasLimit).mul(gasPrice)
+                    // console.log('Tx gas price: ', ethers.utils.formatEther(maxGasFee));
+
+                    const tx1 = await token.transfer(to, balance,
+                        {
+                            gasLimit: gasLimit
+                        }
+                    )
+                    const tx = await tx1.wait()
+                    console.log(`tx success :) | tx hash ${tx.hash}`)
+
+                } catch (error) {
+                    console.log(error)
+                }
+               
+            }
+        }
+    })
+}
+
+bot()
+
+// https://etherscan.io/tx/0x5e4c111da41b7c0058f180186b3f259c113537fe4cd7df907123b8bdfa57e807
