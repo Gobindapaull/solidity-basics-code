2b83770d074f0a37f95cc13c64bb97d579cc17f3
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-07-21 07:21:02 +0530

    Create deploy.js

---
+++ 2025/July/Lottery/deploy.js
@@ -0,0 +1,16 @@
+const ethers = require("ethers");
+require("dotenv").config();
+
+const ABI = require("./build/Lottery.json");
+const bytecode = require("./build/Lottery.bytecode.json");
+
+async function deploy() {
+    const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+
+    const factory = new ethers.ContractFactory(ABI, bytecode.bytecode, wallet);
+    const contract = await (await factory.deploy()).waitForDeployment();
+    console.log(`Deployed contract address: ${await contract.getAddress()}`);
+}
+
+deploy();
