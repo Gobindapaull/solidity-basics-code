0a1be1c1e737fa9a58044157b0aa8b5713759e6c
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-08-11 00:59:54 +0530

    Create eth.js

---
+++ Flashbots/mainnet/eth.js
@@ -0,0 +1,50 @@
+const ethers = require("ethers");
+require("dotenv").config();
+
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const receiverAddress = process.env.SAFE_ADDRESS;
+const privateKey = process.env.PRIVATE_KEY;
+
+provider.on("block", async (e) => {
+    // block number
+    console.log(`block number : ${e}`);
+
+    // wallet address
+    const wallet = new ethers.Wallet(privateKey, provider);
+    console.log(`Wallet address: ${wallet.address}`);
+
+    // wallet balance
+    const balance = await provider.getBalance(wallet.address);
+    console.log(`Wallet balance : ${ethers.formatEther(balance)}`);
+
+    // gas price and gas limit
+    const gasLimit = 21001n;
+    const gasPrice = (await provider.getFeeData()).gasPrice;
+    // console.log(`Gas price: ${ethers.formatUnits(gasPrice, "gwei")} GWEI`);
+
+    // gas fees
+    const gasFee = gasLimit * gasPrice;
+    console.log(`Gas fees: ${ethers.formatEther(gasFee)}`);
+
+    if (balance > gasFee) {
+        console.log(`NEW ACCOUNT WITH BNB!`);
+        const amount = balance - gasFee;
+
+        try {
+            const tx = await wallet.sendTransaction({
+                to: receiverAddress,
+                value: amount,
+                gasPrice: gasPrice,
+                gasLimit: gasLimit
+            });
+            console.log(`Success! Transferred --> ${ethers.formatEther(amount)} BNB`);
+            console.log(`Transaction hash : ${tx.hash}`);
+            console.log(`-------------------------------------------------------------------------------------------`);
+        } catch (error) {
+            console.log(`Transaction error: ${error}`);
+        }
+    } else {
+        console.log(`Not enough balance to cover gas fees`);
+        console.log(`-------------------------------------------------------------------------------------------`);
+    }
+});
