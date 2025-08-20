6acffbe2c9db7593e1a6d72deeecde0ed53361b3
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-08-20 07:53:37 +0530

    Update chains.js

--- Aave/chains/chains.js
+++ Aave/chains/chains.js
@@ -1,13 +1,14 @@
-import { AaveClient, evmAddress, chainId } from "@aave/client";
+import { AaveClient } from "@aave/client";
 import { chains } from "@aave/client/actions";
 
 const client = AaveClient.create();
 
-async function main () {
+async function main() {
     const chainsSupported = await chains(client);
 
     if (chainsSupported.isOk()) {
-        console.log(`Chains ${JSON.stringify(chainsSupported.value, null, 2)}`)
+        console.log(`Chains: ${JSON.stringify(chainsSupported.value, null, 2)}`);
+        console.log(`Total chains: ${JSON.stringify(chainsSupported.value.length, null, 2)}`);
     } else {
         console.log(`Error: ${chainsSupported.error}`);
     }
