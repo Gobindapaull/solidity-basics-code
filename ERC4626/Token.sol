1764aa040719478a98eba65512a067dff355b31e
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-08-21 08:04:48 +0530

    Update Token.sol

--- ERC4626/Token.sol
+++ ERC4626/Token.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("Test Token", "TST") {
+        _mint(msg.sender, 10000000 * 10 ** decimals()); // 10 million tokens
+    }
+}
 
