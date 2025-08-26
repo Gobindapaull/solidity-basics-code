74cac230909ce38407b273c7617d4e96e4a5758b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-08-26 15:22:05 +0530

    Update id.js

--- NPM Package/crypto/id.js
+++ NPM Package/crypto/id.js
@@ -5,3 +5,6 @@ console.log(hexId); // 031a85582d00b821
 
 const id = hexId.slice(0, 8).toUpperCase();
 console.log(id); // 608F20AA
+
+const datePart = new Date().toISOString().replace(/[^0-9]/g, "").slice(0, 14);
+console.log(datePart); // 20250826095142
