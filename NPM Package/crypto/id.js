1dbd62bd175daca61e03ee7e7273be6ea18d605d
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-08-26 15:00:13 +0530

    Update id.js

--- NPM Package/crypto/id.js
+++ NPM Package/crypto/id.js
@@ -1,4 +1,7 @@
 const crypto = require("crypto");
 
-const id = crypto.randomBytes(8).toString("hex").slice(0, 8).toUpperCase();
+const hexId = crypto.randomBytes(8).toString("hex");
+console.log(hexId); // 031a85582d00b821
+
+const id = hexId.slice(0, 8).toUpperCase();
 console.log(id); // 608F20AA
