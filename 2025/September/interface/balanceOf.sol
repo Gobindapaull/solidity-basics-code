2377e6b5226d07588b77455ebed5e6e5e1e5215f
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-09-06 22:31:17 +0530

    Update balanceOf.sol

--- 2025/September/interface/balanceOf.sol
+++ 2025/September/interface/balanceOf.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+}
+
+contract MyContract {
+    address public tokenAddress;
+
+    constructor(address _tokenAddress) {
+        tokenAddress = _tokenAddress;
+    }
+
+    function getBalance(address _addr) external view returns (uint256) {
+        return IERC20(tokenAddress).balanceOf(_addr);
+    }
+}
