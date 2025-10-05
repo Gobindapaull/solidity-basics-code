59554abe0e79672ca67ca646309e57503e345d9d
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-10-05 14:11:15 +0530

    Create hardhat.config.ts

---
+++ FLASH USDT/hardhat.config.ts
@@ -0,0 +1,28 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    }
+  },
+};
+
+export default config;
