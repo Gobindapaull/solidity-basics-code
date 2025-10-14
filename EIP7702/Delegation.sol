417f4d24661ad70ee2d950f16ff704bbd7e54ab1
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-10-14 08:26:40 +0530

    Update Delegation.sol

--- EIP7702/Delegation.sol
+++ EIP7702/Delegation.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract Delegation {
+    address public mainWallet;
+
+    constructor(address _mainWallet) {
+        mainWallet = _mainWallet;
+    }
+
+    // Automatically forward ETH upon receiving
+    receive() external payable {
+        _forward();
+    }
+
+    // Manual trigger if auto-forward fails for any reason
+    function forward() external {
+        _forward();
+    }
+
+    function _forward() internal {
+        uint256 amount = address(this).balance;
+        if (amount > 0) {
+            (bool ok, ) = mainWallet.call{value: amount}("");
+            require(ok, "Forward failed");
+        }
+    }
+}
 
