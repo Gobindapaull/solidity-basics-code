b56530d558b90b2fb8adf875c4e37fe2016210c9
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2025-12-09 00:50:48 +0530

    Update ContractB.sol

--- 2025/December/CallContract/ContractB.sol
+++ 2025/December/CallContract/ContractB.sol
@@ -1,1 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.31;
 
+import "./ContractA.sol";
+
+contract MainContract {
+    ContractA public contractA;
+
+    constructor(address _contractAddress) {
+        contractA = ContractA(_contractAddress);
+    }
+
+    function addNumber(uint256 _num) public {
+        contractA.store(_num);
+    }
+}
