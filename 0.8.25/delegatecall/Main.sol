6d594e9a79f1d3af8bc7aa1ae260bcf6f471d98b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-03-12 14:13:54 +0530

    Update Main.sol

--- 0.8.25/delegatecall/Main.sol
+++ 0.8.25/delegatecall/Main.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.34;
 
+contract Main {
+    address public delegateContract;
+    address[] public previousDelegates;
+
+    uint256 public total;
+    event DelegateChanged(address oldAddress, address newAddress);
+
+    constructor(address _delegateContract) {
+        delegateContract = _delegateContract;
+    }
+
+    function changeDelegateContract(address _newDelegate) public returns (bool) {
+        if(_newDelegate != delegateContract) {
+            previousDelegates.push(delegateContract);
+            address oldDelegate = delegateContract;
+            delegateContract = _newDelegate;
+            emit DelegateChanged(oldDelegate, _newDelegate);
+            return true;
+        }
+        return false;
+    }
+
+    function delegateAdd(uint256 num1, uint256 num2) public {
+        (bool success, ) = delegateContract.delegatecall(abi.encodeWithSignature("add(uint256,uint256)", num1, num2));
+        require(success, "delegateAdd failed");
+    }
+
+    function callAdd(uint256 num1, uint256 num2) public {
+        (bool success, ) = delegateContract.call(abi.encodeWithSignature("add(uint256,uint256)", num1, num2));
+        require(success, "callAdd failed");
+    }
+}
