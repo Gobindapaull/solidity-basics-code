4bfbc8ff715c25ec4475e78773342a1bc51143d7
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-05-16 07:32:52 +0530

    Update AutoSendETH.sol

--- contract/AutoSend/AutoSendETH.sol
+++ contract/AutoSend/AutoSendETH.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.35;
 
+contract AutoSend {
+    address payable public receiver;
+
+    constructor(address payable _receiver) {
+        receiver = _receiver;
+    }
+
+    receive() external payable {
+        (bool success, ) = receiver.call{value: msg.value}("");
+        require(success, "Failed to send ETH");
+    }
+}
