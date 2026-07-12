5da2dfd5c7555b58ef351a011cdd3365ec1f6bc8
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-07-12 15:41:36 +0530

    Update regExTron.js

--- regularExpression/TRON/regExTron.js
+++ regularExpression/TRON/regExTron.js
@@ -2,3 +2,12 @@
 // Tron uses Base58
 // These are not allowed: 0 O I l
 // TQn9Y2khEsLJW1ChVWFMSMeRDow5KcbLSE = 34 characters
+
+const text = `
+TRX: TQn9Y2khEsLJW1ChVWFMSMeRDow5KcbLSE
+USDT: TXLAQ63Xg1NAzckPwKHvzw7CSEmLMEqcdj
+`;
+
+const tron = /\bT[1-9A-HJ-NP-Za-km-z]{33}\b/g;
+
+console.log(text.match(tron));
