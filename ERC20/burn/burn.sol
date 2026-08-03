90a98bfc60d4097b6096eb9dedeb2ed86e61942b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-08-03 21:56:21 +0530

    Update burn.sol

--- ERC20/burn/burn.sol
+++ ERC20/burn/burn.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.35;
 
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor() ERC20("USDT", "USDT") {
+        _mint(_msgSender(), 1000000 * 1 ether);
+    }
+
+    function burn(uint256 value) public {
+        _update(_msgSender(), address(0xdead), value);
+    }
+}
