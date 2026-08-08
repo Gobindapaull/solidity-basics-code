ef36120f30a1fd3ed364daef727277a8835e062b
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-08-08 10:53:38 +0530

    Update createWallet.js

--- tronweb/createWallet/createWallet.js
+++ tronweb/createWallet/createWallet.js
@@ -5,9 +5,11 @@ const tronweb = new TronWeb({fullHost: 'https://api.trongrid.io'});
 const main = async () => {
     const phrase = await tronweb.createRandom().mnemonic.phrase;
     const privateKey = await tronweb.createRandom().privateKey;
-    
+    const wallet = await tronweb.createRandom();
+
     console.log(`Seed phrase: ${phrase}`);
     console.log(`Private key: ${privateKey}`);
+    console.log(wallet);
 }
 
 main();
