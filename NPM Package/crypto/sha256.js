3c38d4425b0b389825df3c75e61ae813a9f7f039
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-08-16 16:12:38 +0530

    Update sha256.js

--- NPM Package/crypto/sha256.js
+++ NPM Package/crypto/sha256.js
@@ -4,3 +4,9 @@ const text = "hello";
 
 const hash = crypto.createHash("sha256").update(text).digest("hex");
 console.log(hash); // 2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824
+
+const bytes8 = crypto.randomBytes(8).toString("hex");
+console.log(bytes8); // 4a46c8a666f4d085
+
+const privateKey = crypto.randomBytes(32).toString("hex");
+console.log(privateKey, privateKey.length);
