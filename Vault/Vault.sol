9a35354d0c04d3afbbc27a28804178e6733d8b95
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-09-03 02:23:26 +0530

    Update Vault.sol

--- Vault/Vault.sol
+++ Vault/Vault.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.36;
 
+contract Vault {
+    address public owner;
+    mapping(address => uint256) public balances;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 _amount) external {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        balances[msg.sender] -= _amount;
+        (bool success, ) = payable(msg.sender).call{value: _amount}("");
+        require(success, "Transfer failed");
+    } 
+}
