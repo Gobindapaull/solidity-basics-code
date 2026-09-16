b366da6eb1e8653772b3978f2e17c7b15742a7f6
Gobinda Paul <80039050+Gobindapaull@users.noreply.github.com>
2026-09-16 08:23:05 +0530

    Create checkRpc.js

---
+++ RPC_URL/checkRpc.js
@@ -0,0 +1,17 @@
+const ethers = require("ethers")
+require("dotenv").config()
+
+async function main() {
+    try {
+        console.log("RPC:", process.env.RPC_URL)
+        const provider = new ethers.JsonRpcProvider(process.env.RPC_URL)
+        const network = await provider.getNetwork()
+
+        console.log("Chain ID:", network.chainId)
+        console.log("Network:", network.name)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+main()
